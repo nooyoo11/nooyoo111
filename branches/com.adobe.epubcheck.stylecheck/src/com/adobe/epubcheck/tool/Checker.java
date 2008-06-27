43bf0e19c5d1e9ede33358f7b35989ec5f6a002a
PiotrFKula <PiotrFKula@ae9e6ba8-4d41-0410-bbe8-1710a195d073>
2008-06-27 21:30:08 +0000

    Integrated style checker

---
+++ branches/com.adobe.epubcheck.stylecheck/src/com/adobe/epubcheck/tool/Checker.java
@@ -0,0 +1,53 @@
+/*
+ * Copyright (c) 2007 Adobe Systems Incorporated
+ *
+ *  Permission is hereby granted, free of charge, to any person obtaining a copy of
+ *  this software and associated documentation files (the "Software"), to deal in
+ *  the Software without restriction, including without limitation the rights to
+ *  use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+ *  the Software, and to permit persons to whom the Software is furnished to do so,
+ *  subject to the following conditions:
+ *
+ *  The above copyright notice and this permission notice shall be included in all
+ *  copies or substantial portions of the Software.
+ *
+ *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ *  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+ *  FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+ *  COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+ *  IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+ *  CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ *
+ *    <AdobeIP#0000474>
+ */
+
+package com.adobe.epubcheck.tool;
+
+import java.io.File;
+
+import com.adobe.epubcheck.api.EpubCheck;
+import com.adobe.epubcheck.api.Report;
+import com.adobe.epubcheck.util.DefaultReportImpl;
+
+public class Checker {
+
+	public static void main(String[] args) {
+		if (args.length != 1) {
+			System.err.println("single argument expected");
+			return;
+		}
+
+		String epubName = args[0];
+		Report report = new DefaultReportImpl(epubName);
+
+		if (!epubName.endsWith(".epub"))
+			report.warning(null, 0, "filename does not include '.epub' suffix");
+
+		EpubCheck check = new EpubCheck(new File(epubName), report);
+		if( check.validate() )
+			System.out.println("No errors or warnings detected");
+		else
+			System.exit(1); // Exit with status code 1 if there were errors reported
+	}
+
+}
