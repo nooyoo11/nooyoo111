bc9d97a4b59941bebb3c37b28b7dd9587fa3dfdd
google@novomail.net <google@novomail.net@ae9e6ba8-4d41-0410-bbe8-1710a195d073>
2011-10-15 01:55:01 +0000

    Converted OCFPackage to an abstract class, pushing methods specific to reading .zip files down to the new OCFZipPackage class.

--- branches/epub3/com.adobe.epubcheck/src/com/adobe/epubcheck/ocf/OCFPackage.java
+++ branches/epub3/com.adobe.epubcheck/src/com/adobe/epubcheck/ocf/OCFPackage.java
@@ -1,23 +1,26 @@
 package com.adobe.epubcheck.ocf;
 
+import java.io.BufferedInputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.util.Enumeration;
 import java.util.HashSet;
 import java.util.Hashtable;
-import java.util.zip.ZipEntry;
-import java.util.zip.ZipFile;
-
+import com.adobe.epubcheck.api.Report;
+import com.adobe.epubcheck.opf.OPFData;
+import com.adobe.epubcheck.opf.OPFHandler;
+import com.adobe.epubcheck.opf.VersionRetriever;
+import com.adobe.epubcheck.opf.XRefChecker;
+import com.adobe.epubcheck.util.EPUBVersion;
 import com.adobe.epubcheck.util.GenericResourceProvider;
+import com.adobe.epubcheck.util.InvalidVersionException;
+import com.adobe.epubcheck.xml.XMLParser;
 
-public class OCFPackage implements GenericResourceProvider {
+public abstract class OCFPackage implements GenericResourceProvider {
 
-	ZipFile zip;
 	Hashtable<String, EncryptionFilter> enc;
 	String uniqueIdentifier;
 
-	public OCFPackage(ZipFile zip) {
-		this.zip = zip;
+	public OCFPackage() {
 		this.enc = new Hashtable<String, EncryptionFilter>();
 	}
 
@@ -25,61 +28,91 @@ public class OCFPackage implements GenericResourceProvider {
 		enc.put(name, encryptionFilter);
 	}
 
-	public void setUniqueIdentifier(String idval) {
-		uniqueIdentifier = idval;
-	}
+    /**
+     * @param name the name of a relative file that is possibly in the container
+     * @return true if the file is in the container, false otherwise
+     */
+    public abstract boolean hasEntry(String name);
 
-	public String getUniqueIdentifier() {
-		return uniqueIdentifier;
-	}
-
-	public boolean hasEntry(String name) {
-		return zip.getEntry(name) != null;
-	}
-
-	public boolean canDecrypt(String name) {
-		EncryptionFilter filter = (EncryptionFilter) enc.get(name);
+    /**
+     * @param name the name of a relative file to fetch from the container.
+     * @return an InputStream representing the data from the named file, possibly
+     * decrypted if an appropriate encryption filter has been set
+     */
+    public abstract InputStream getInputStream(String name) throws IOException;
+    
+    /**
+     * 
+     * @return a set of relative file names of files in this container
+     * @throws IOException
+     */
+    public abstract HashSet<String> getFileEntries() throws IOException;
+    
+    /**
+     * 
+     * @return a set of relative directory entries in this container
+     * @throws IOException
+     */
+    public abstract HashSet<String> getDirectoryEntries() throws IOException;
+    
+    
+    /**
+     * 
+     * @param name
+     * @return true if I have an Encryption filter for this particular file.
+     */
+	public boolean canDecrypt(String fileName) {
+		EncryptionFilter filter = (EncryptionFilter) enc.get(fileName);
 		if (filter == null)
 			return true;
 		return filter.canDecrypt();
 	}
 
-	public InputStream getInputStream(String name) throws IOException {
-		ZipEntry entry = zip.getEntry(name);
-		if (entry == null)
-			return null;
-		InputStream in = zip.getInputStream(entry);
-		EncryptionFilter filter = (EncryptionFilter) enc.get(name);
-		if (filter == null)
-			return in;
-		if (filter.canDecrypt())
-			return filter.decrypt(in);
-		return null;
-	}
+    /**
+     * This method parses the container entry and stores important data, but does /not/
+     * validate the container against a schema definition.
+     * @param reporter a Report instance where errors are reported
+     * @return an OCFHandler instance, cast to the OCFData interface
+     */
+    public OCFData getOcfData( Report reporter )
+    {
+        XMLParser containerParser = null;
+        try {
+            containerParser = new XMLParser(getInputStream(OCFData.containerEntry),
+                    OCFData.containerEntry, "xml", reporter, null);
+            OCFHandler containerHandler = new OCFHandler(containerParser);
+            containerParser.addXMLHandler(containerHandler);
+            containerParser.process();
+            return containerHandler;
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
 
-	public HashSet<String> getFileEntries() throws IOException {
-		HashSet<String> entryNames = new HashSet<String>();
+    
+    /**
+     * This method parses the .opf file contained in an OCFContainer and stores important data,
+     * but does /not/ validate the opf file against a schema definition.
+     * @param container the OCFData container which holds the container.xml data
+     * @param reporter a Report instance where errors are reported
+     * @return an OPFHandler instance, cast to the OPFData interface
+     * @throws InvalidVersionException if the 'version' attribute in the <package> element
+     *          is not "2.0" or "3.0"
+     * @throws IOException for any other io error.
+     */
+    public OPFData getOpfData( OCFData container, Report reporter ) 
+            throws InvalidVersionException, IOException
+    {
+        String path = container.getRootPath();
+        EPUBVersion version = new VersionRetriever(path, reporter)
+                .retrieveOpfVersion( getInputStream( path ));
 
-		for (Enumeration entries = zip.entries(); entries.hasMoreElements();) {
-			ZipEntry entry = (ZipEntry) entries.nextElement();
-			if (!entry.isDirectory()) {
-				entryNames.add(entry.getName());
-			}
-		}
+        XMLParser opfParser = new XMLParser(new BufferedInputStream(
+                getInputStream( path )), path, "opf",
+                reporter, version);
 
-		return entryNames;
-	}
-
-	public HashSet<String> getDirectoryEntries() throws IOException {
-		HashSet<String> entryNames = new HashSet<String>();
-
-		for (Enumeration entries = zip.entries(); entries.hasMoreElements();) {
-			ZipEntry entry = (ZipEntry) entries.nextElement();
-			if (entry.isDirectory()) {
-				entryNames.add(entry.getName());
-			}
-		}
-
-		return entryNames;
-	}
+        return new OPFHandler( this, path, reporter,
+                new XRefChecker(this, reporter, version), opfParser, version);
+    }
+    
 }
